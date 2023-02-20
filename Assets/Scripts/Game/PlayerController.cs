eae9763fb1660ffdddb102fad9c6f79b9a8268d8
Sebastian Siljuholtet Johansen <aurilisdev@gmail.com>
2023-02-20 18:54:08 +0100

    Push scenes and controller

---
+++ Assets/Scripts/Game/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _body;
+    
+    [SerializeField] private float movementSpeed = 7f;
+
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void Update()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        float dirY = Input.GetAxisRaw("Vertical");
+        _body.velocity = new Vector2(dirX * movementSpeed, _body.velocity.y);
+        _body.velocity = new Vector2( _body.velocity.x, dirY * movementSpeed); 
+    }
+}
