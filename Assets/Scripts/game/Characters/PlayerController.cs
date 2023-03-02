73afe4123de4e301c9c329df8457926ef16abcf9
isakaanonli <isakaanonli@gmail.com>
2023-03-02 19:40:27 +0100

    Health and enemy test

--- Assets/Scripts/game/Characters/PlayerController.cs
+++ Assets/Scripts/game/Characters/PlayerController.cs
@@ -5,12 +5,15 @@ namespace TLOU2D.game {
     public class PlayerController : MonoBehaviour
     {
         private Rigidbody2D _body;
-    
+        private BoxCollider2D _col;
+
+        [SerializeField] private int health = 100;
         [SerializeField] private float movementSpeed = 7f;
 
         void Start()
         {
             _body = GetComponent<Rigidbody2D>();
+            _col = GetComponent<BoxCollider2D>();
         }
 
         void FixedUpdate()
@@ -20,5 +23,16 @@ namespace TLOU2D.game {
             _body.velocity = new Vector2(dirX * movementSpeed, _body.velocity.y);
             _body.velocity = new Vector2( _body.velocity.x, dirY * movementSpeed); 
         }
+        
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.gameObject.CompareTag("EnemyTest"))
+            {
+                health -= 10;
+                Debug.Log(health);
+
+            }
+        }
     }
-}
+    
+    }
