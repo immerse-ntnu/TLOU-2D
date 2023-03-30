846f8169288160708d57abb097f4ca07ba2596cf
Sebastian Siljuholtet Johansen <aurilisdev@gmail.com>
2023-03-30 19:01:11 +0200

    Changes

--- Assets/Scripts/game/world/Chunk.cs
+++ Assets/Scripts/game/world/Chunk.cs
@@ -1,28 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
-public class Chunk : MonoBehaviour
-{
-    public int gridX { get; private set; }
-
-    public int gridY { get; private set; }
-
-    public SpriteRenderer renderReference { get; private set; }
-
-    public Chunk(int x, int y, SpriteRenderer renderer)
+namespace TLOU2D.game.world{
+    public class Chunk
     {
-        gridX = x;
-        gridY = y;
-        renderReference = renderer;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        public static float width = (float)(5.12f * 0.33f / Math.Sqrt(2.0));
+        public static float height = width;
+        
+        public int gridX { get; private set; }
+
+        public int gridY { get; private set; }
+
+        public SpriteRenderer renderReference { get; private set; }
+
+        public Collider2D collider;
+        
+        public Chunk(int x, int y, SpriteRenderer renderer)
+        {
+            gridX = x;
+            gridY = y;
+            renderReference = renderer;
+        }
+
+        public bool isWithinChunks(Transform transform, int chunksAway)
+        {
+            float diffX = transform.position.x - (gridX - 2) * width;
+            float diffY = transform.position.y - (gridY - 2) * height;
+            return Math.Abs(diffX) / width <= chunksAway && Math.Abs(diffY) / height <= chunksAway;
+        }
     }
 }
--- Assets/Scripts/game/world/World.cs
+++ Assets/Scripts/game/world/World.cs
@@ -26,24 +26,30 @@ namespace TLOU2D.game.world
             LoadChunks();
         }
 
+        public static int width = 20;
+        public static int height = 20;
+
         void LoadChunks()
         {
-            int width = 50;
-            int height = 50;
-            float gap = 0.05f;
-            float halfwidth = 5.7f + gap;
-            float halfheight = 5.7f / Mathf.Sqrt(2.0f) + gap;
-            for (int i = 0; i < width; i++)
+
+            for (int i = -width / 2; i <= width / 2; i++)
             {
-                for (int j = 0; j < height; j++)
+                for (int j = -height / 2; j <= height / 2; j++)
                 {
-                    Vector3 pos = new Vector3(halfwidth * (i - width  / 2), halfheight * (j - height  / 2), 10);
-                    SpriteRenderer renderer = Instantiate(grass_tile_prefab, tiles_holder);
-                    renderer.transform.position = pos;
-                    chunks.Add(new Chunk(i, j, renderer));
+                    int trueGridX = (i - width / 2);
+                    int trueGridY = (j - height / 2);
+                    if (!(trueGridX == 0 && trueGridY == 0))
+                    {
+                        // may remove when we launch game.
+                        Vector3 pos = new Vector3(Chunk.width * trueGridX, Chunk.height * trueGridY, 10);
+                        SpriteRenderer renderer = Instantiate(grass_tile_prefab, tiles_holder);
+                        renderer.transform.position = pos;
+                        Chunk newChunk = new Chunk(i, j, renderer);
+                        chunks.Add(newChunk);
+                    }
                 }
+
             }
-            
         }
 
         public void pauseWorld(bool val)
@@ -56,9 +62,54 @@ namespace TLOU2D.game.world
         // Update is called once per frame
         private void Update()
         {
+            List<Chunk> keep = new List<Chunk>();
+            int gridOffsetX = (int)(player.position.x / Chunk.width);
+            int gridOffsetY = (int)(player.position.y / Chunk.height);
+            for (int i = -width / 2; i <= width / 2; i++)
+            {
+                for (int j = -height / 2; j <= height / 2; j++)
+                {
+                    int realI = i + gridOffsetX;
+                    int realJ = j + gridOffsetY;
+                    Chunk found = null;
+                    foreach (Chunk chunk in chunks)
+                    {
+                        if (chunk.gridX == realI && chunk.gridY == realJ)
+                        {
+                            found = chunk;
+                            keep.Add(found);
+                        }
+                    }
+
+                    if (found is null)
+                    {
+                        // may remove when we launch game.
+                        Vector3 pos = new Vector3(Chunk.width * realI, Chunk.height * realJ, 10);
+                        SpriteRenderer renderer = Instantiate(grass_tile_prefab, tiles_holder);
+                        renderer.transform.position = pos;
+                        Chunk newChunk = new Chunk(realI, realJ, renderer);
+                        chunks.Add(newChunk);
+                        keep.Add(newChunk);
+                    }
+                }
+            }
+
+            List<Chunk> forRemoval = new List<Chunk>();
             foreach (Chunk chunk in chunks)
             {
-                chunk.renderReference.transform.position = new Vector3(chunk.renderReference.transform.position.x, chunk.renderReference.transform.position.y, player.position.z + 10);
+                chunk.renderReference.transform.position = new Vector3(
+                    chunk.renderReference.transform.position.x, chunk.renderReference.transform.position.y,
+                    player.position.z + 10);
+                if (!keep.Contains(chunk))
+                {
+                    forRemoval.Add(chunk);
+                }
+            }
+
+            foreach (Chunk chunk in forRemoval)
+            {
+                chunks.Remove(chunk);
+                Destroy(chunk.renderReference);
             }
         }
     }
